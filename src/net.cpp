c0d924aedc59ec7e476c04c78d0d8c25281f4a81
Sambhav Sharma <sambhav.sharma@hotmail.com>
2017-09-17 13:46:23 +0530

    adding new seed addresses

--- src/net.cpp
+++ src/net.cpp
@@ -1236,9 +1236,9 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"seed1", "45.55.78.241"},
-	{"seed2", "45.55.185.61"},
-	{"seed3", "162.243.169.199"},
+    {"seed1", "seed1.nlc2.info"},
+	{"seed2", "seed2.nlc2.info"},
+	{"seed3", "seed3.nlc2.info"},
 
 };
 
