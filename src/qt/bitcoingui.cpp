b7bfbca9669abbc3fce7d894ef7d12fc4cb8f335
Sambhav Sharma <sambhav.sharma@hotmail.com>
2017-09-24 06:50:54 +0530

    setting minimum size of the wallet

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -80,6 +80,7 @@ BitcoinGUI::BitcoinGUI(QWidget *parent):
     rpcConsole(0),
     nWeight(0)
 {
+    setMinimumSize(970,500);
     resize(970,500);
 
 	QFontDatabase::addApplicationFont(":/fonts/Bebas");
