484bdea8a2511a1c363af4ada475f07546f2538e
unblocktechie <76834820+unblocktechie@users.noreply.github.com>
2021-03-31 10:23:32 +0530

    Create index.js

---
+++ BEP20/NLC_Burn/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
